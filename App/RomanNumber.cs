f25bba07e30acc6e3599ddd4cc8835cb2d440221
neonmag <neonmag@DESKTOP-OGBC7DT>
2023-09-01 16:20:45 +0300

    Add project files.

---
+++ App/RomanNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class RomanNumber
+    {
+        public int Value { get; set; }
+        public int Val2;
+        public static RomanNumber Parse(String input)
+        {
+            return new()
+            {
+                Value = 1
+            };
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using App; // додати залежність (Dependency) від проєкту App
+namespace Tests
+{
+    [TestClass]
+    public class UnitTestApp
+    {
+        [TestMethod]
+        public void TestRomanNumberParse()
+        {
+            Assert.AreEqual(            // RomanNumber.Parse("I").Value == 1
+                1,                      // Значення, що очікується (що має бути, правильний варіант)
+                RomanNumber             // Актуальнее значення (те, що вирахуване)
+                    .Parse("I")         //  
+                    .Value,             //
+                "1 == I"                //
+                );                      // Повідомленя, що з'явиться при провалі тесту
+            
+        }
+    }
+}
+/* Основу модульних тестів складають твердження (Asserts).
+ * У твердженні фігурують два значення: те, що очікується, та
+ * те, що одержується.
+ * Більшість тестів перевіряють рівність (об'єктну AreSame чи контентну AreEqual),
+ * або у скороченій формі (IsTrue, IsNotNull, ....)
+ */
