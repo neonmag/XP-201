9feff6f33354b7b6aeee447fda15cc8b853359fc
Boris Neonmag <gamerisukrainer@gmail.com>
2023-09-04 13:09:58 +0300

    RomanNumbers calculating

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -11,8 +11,52 @@ namespace App
         public int Value { get; set; }
         public static RomanNumber Parse(String input)
         {
-            if(input == "I") return new() {Value = 1};
-            else return new() {Value = 2};
+            if (String.IsNullOrEmpty(input))
+            {
+                throw new ArgumentNullException("Null or empty input");
+            }
+            if (input == "N") return new();
+            int lastDigitIndex = input[0] == '-' ? 1 : 0;
+            int prev = 0;
+            int current;
+            int result = 0;
+            for (int i = input.Length - 1; i >= lastDigitIndex; i--)
+            {
+                current = input[i] switch
+                {
+                    'I' => 1,
+                    'V' => 5,
+                    'X' => 10,
+                    'L' => 50,
+                    'C' => 100,
+                    'D' => 500,
+                    'M' => 1000,
+                };
+                result += prev > current ? -current : current;
+                prev = current;
+            }
+            return new()
+            {
+                Value = result * (1 - 2 * lastDigitIndex)
+            };
+            //input.Length для тестів
+            /*input switch 
+            { 
+                "I" => 1,
+                "II" => 2,
+                _ => 3,
+            } 
+            //input == "I" ? 1 : 2*/
+            /* Правило "читання" римських чисел :
+             * Якщо цифра передує більшій цифрі, то вона віднімається (IV, IX)
+             * Меншій, або рівній - додається (VI, XI, II)
+             * Решту правил ігноруємо - робимо максимально "дружній" інтерфейс
+             * 
+             * Алгоритм - "заходимо" з правої цифри, її завжди додаємо, запам'ятовуємо
+             * і далі порівнюємо з наступною цифрою.
+             */
         }
     }
 }
+
+
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -4,10 +4,92 @@ namespace Tests
     [TestClass]
     public class UnitTestApp
     {
+        private static Dictionary<String, int> parseTests = new()
+        {
+            { "I"          , 1    },
+            { "II"         , 2    },
+            { "III"        , 3    },
+            { "IIII"       , 4    }, // Особливі твердження - з них ми визначаємо про
+            { "IV"         , 4    }, // підтримку неоптимальних записів чисел
+            { "V"          , 5    },
+            { "VI"         , 6    },
+            { "VII"        , 7    },
+            { "VIII"       , 8    },
+            { "IX"         , 9    },
+            { "X"          , 10   },
+            { "VV"         , 10   },  // Ще одне наголошення неоптимальності
+            { "IIIIIIIIII" , 10   },  // Ще одне наголошення неоптимальності
+            { "VX"         , 5    },  // Ще одне наголошення неоптимальності
+            { "N"          , 0    },  // Доповнюємо множину чисел нулем
+            { "-L"         , -50  },
+            { "-XL"        , -40  },
+            { "-IL"        , -49  },  // Неоптимальність
+            { "-D"         , -500 },
+            { "-C"         , -100 },
+            { "-M"         , -1000 },
+            { "D"          , 500 },
+            { "C"          , 100 },
+            { "M"          , 1000 },
+            { "IM"         , 999 },
+            { "-IM"        , -999 },
+            { "IC"         , 99 },
+            { "-IC"        , -99 },
+            { "ID"         , 499 },
+            { "-ID"        , -499 },
+            { "VM"         , 995 },
+            { "-VM"        , -995 },
+            { "VC"         , 95 },
+            { "-VC"        , -95 },
+            { "VD"         , 495 },
+            { "-VD"        , -495 },
+            { "XM"         , 990 },
+            { "-XM"        , -990 },
+            { "XC"         , 90 },
+            { "-XC"        , -90 },
+            { "XD"         , 490 },
+            { "-XD"        , -490 },
+            { "MI"         , 1001 },
+            { "-MI"        , -1001 },
+            { "CI"         , 101 },
+            { "-CI"        , -101 },
+            { "DI"         , 501 },
+            { "-DI"        , -501 },
+            { "MV"         , 1005 },
+            { "-MV"        , -1005 },
+            { "CV"         , 105 },
+            { "-CV"        , -105 },
+            { "DV"         , 505 },
+            { "-DV"        , -505 },
+            { "MX"         , 1010 },
+            { "-MX"        , -1010 },
+            { "CX"         , 110 },
+            { "-CX"        , -110 },
+            { "DX"         , 510 },
+            { "-DX"        , -510 },/////////////  
+            {"LX"          , 60 },
+            {"LXII"        , 62 },
+            {"CCL"         , 250 },
+            {"-CCIII"      , -203 },
+            {"-LIV"        ,  -54},
+            {"MDII"        , 1502 },
+            //{"-CCM"        , -800 },
+            {"DD"          ,1000 },
+            {"CCCCC"       ,500 },
+            {"IVIVIVIVIV"  ,20 },
+            {"CDIV"        ,404},
+            { "CCCC"         , 400 },
+            { "LM"         , 950 },
+            { "CDX"         , 410 },
+            { "DDD"         , 1500 },
+            { "MDCC"         , 1700 },
+            { "DDDIV"         , 1504 },
+            { "MMMM"         , 4000 },
+        };
+
         [TestMethod]
         public void TestRomanNumberParse()
         {
-            Assert.AreEqual(            // RomanNumber.Parse("I").Value == 1
+            /*Assert.AreEqual(            // RomanNumber.Parse("I").Value == 1
                 1,                      // Значення, що очікується (що має бути, правильний варіант)
                 RomanNumber             // Актуальнее значення (те, що вирахуване)
                     .Parse("I")         //  
@@ -21,7 +103,24 @@ namespace Tests
                     .Value,
                 "2 == II"
                 );
-            
+            Assert.AreEqual(
+                3,
+                RomanNumber
+                    .Parse("III")
+                    .Value,
+                "3 == III"
+                );*/
+            foreach (var pair in parseTests)
+            {
+                Assert.AreEqual(
+               pair.Value,
+               RomanNumber
+                   .Parse(pair.Key)
+                   .Value,
+               $"{pair.Key} == {pair.Value}"
+               );
+            }
+
         }
     }
 }
